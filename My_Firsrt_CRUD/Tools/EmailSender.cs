256f0a3527ad2944ca2bf001fa8c429ada4e2e6f
amirmohammad <rafieyg2708@gmail.com>
2026-07-04 10:25:37 +0330

    Write-EmailSender-Sevice-By-FluentEmail(Package)

---
+++ My_Firsrt_CRUD/Tools/EmailSender.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using FluentEmail.Core;
+using FluentEmail.Smtp;
+
+namespace My_Firsrt_CRUD.Tools
+{
+    public interface IEmailSender
+    {
+        public  Task SendEmailAsync(EmailModel model);
+    }
+
+    public class EmailSender : IEmailSender
+    {
+
+        public async Task SendEmailAsync(EmailModel model)
+        {
+            var sender = new SmtpSender(() => new SmtpClient("smpt.gmail.com")
+            {
+                UseDefaultCredentials = false,
+                Port = 587,
+                Credentials = new NetworkCredential("rafieyg2708@gmail.com", "zhiq ylxm dtrb sioe")
+            });
+
+            Email.DefaultSender = sender;
+
+            var email = await Email
+                 .From("rafieyg2708@gmail.com", "تایید ایمیل")
+                 .To(model.To)
+                 .Subject(model.Subject)
+                 .Body(model.Body, isHtml: true)
+                 .SendAsync();
+
+
+            if (!email.Successful)
+            {
+                foreach (var err in email.ErrorMessages)
+                    Console.WriteLine(err);
+            }
+            
+        }
+
+    }
+    public class EmailModel
+    {
+
+        public EmailModel(string to, string subject, string body)
+        {
+            To = to;
+            Subject = subject;
+            Body = body;
+        }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+
+
+}
