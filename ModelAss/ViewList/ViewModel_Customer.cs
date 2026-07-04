580740ecd747eeafcd0d0d823440823909372b24
amirmohammad <rafieyg2708@gmail.com>
2026-07-04 10:27:13 +0330

    Edit-ViewModel-For-SignUp&Login

--- ModelAss/ViewList/ViewModel_Customer.cs
+++ ModelAss/ViewList/ViewModel_Customer.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ModelAss.ViewList
 {
     public class ViewModel_SignCustomer
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
+        [Required(ErrorMessage ="نام کاربری الزامی هست"),MaxLength(128)]
+        public string UserName { get; set; }
+        
+        [Required(ErrorMessage ="ایمیل الزامی هست"),MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^09\d{9}$",ErrorMessage ="فرمت تلفن صحیح نیست")]
         public string Phone { get; set; }
+        
+        [Required]
         public string Password { get; set; }
     }
     public class ViewModel_LoginCustomer
     {
-        public string Phone { get; set; }
+        [Required(ErrorMessage = "نام کاربری الزامی هست"), MaxLength(128)]
+        public string UserName { get; set; }
+        
+        [Required]
         public string Password { get; set; }
     }
 }
